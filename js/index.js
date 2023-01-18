5b07e98383532cae98775d94970706f56a189077
Sergii Tarasovskyi <neonagat@gmail.com>
2023-01-18 11:46:20 -0500

    Added 'Message form' and 'Messages' sections

--- js/index.js
+++ js/index.js
@@ -15,3 +15,32 @@ for (let i = 0; i < skills.length; i++) {
   skill.innerText = skills[i];
   skillsList.appendChild(skill);
 }
+
+const messageForm = document.querySelector('[name="leave_message"]');
+
+messageForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const name = e.target.name.value;
+  const email = e.target.email.value;
+  const message = e.target.message.value;
+  console.log(name, email, message);
+
+  const messageSection = document.getElementById("messages");
+  const messageList = messageSection.querySelector("ul");
+  const newMessage = document.createElement("li");
+  newMessage.innerHTML = ` <a href=mailto:${email} > ${name} </a> wrote: ${message} `;
+
+  const removeButton = document.createElement("button");
+  removeButton.innerText = "remove";
+  removeButton.type = "button";
+
+  removeButton.addEventListener("click", () => {
+    let entry = removeButton.parentNode;
+    entry.remove();
+  });
+
+  messageList.appendChild(newMessage);
+  newMessage.appendChild(removeButton);
+
+  messageForm.reset();
+});
